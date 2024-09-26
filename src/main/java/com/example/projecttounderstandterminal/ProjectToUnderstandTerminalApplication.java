73e194d98e15eb46133f7f99499788a56001cc83
Chris Ye <chye0001@kea.stud.dk>
2024-09-26 09:32:43 +0200

    initial commit

---
+++ src/main/java/com/example/projecttounderstandterminal/ProjectToUnderstandTerminalApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttounderstandterminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToUnderstandTerminalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectToUnderstandTerminalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projecttounderstandterminal/ProjectToUnderstandTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecttounderstandterminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToUnderstandTerminalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
