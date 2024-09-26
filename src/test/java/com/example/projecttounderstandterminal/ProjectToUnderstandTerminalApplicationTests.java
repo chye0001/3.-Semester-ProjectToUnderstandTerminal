9faa17aa942cc4d9be2a0ae6e0b62a33dc88d224
Chris Ye <chye0001@kea.stud.dk>
2024-09-26 09:43:27 +0200

    disabled test

--- src/test/java/com/example/projecttounderstandterminal/ProjectToUnderstandTerminalApplicationTests.java
+++ src/test/java/com/example/projecttounderstandterminal/ProjectToUnderstandTerminalApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.projecttounderstandterminal;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjectToUnderstandTerminalApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.projecttounderstandterminal;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjectToUnderstandTerminalApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
